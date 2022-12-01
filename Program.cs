ba95a0e2076b4c2dfd3752b238cec58100ddb5d8
Ashot <adres1991@internet.ru>
2022-12-01 19:37:36 +0300

    третья задача

--- Program.cs
+++ Program.cs
@@ -37,3 +37,17 @@ Console.WriteLine("Введите 3 числа:");
             break;
 
     
+
+
+    Console.WriteLine("Введите число:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + num + "является: ЧЁТНЫМ");
+            }
+            
