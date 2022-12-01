6731a89201f08671469c6692d5a47ac977d2c8d7
Ashot <adres1991@internet.ru>
2022-12-01 19:36:36 +0300

    вторая задача

--- Program.cs
+++ Program.cs
@@ -12,3 +12,28 @@ writeline (y " - меньше");
 else y>x
 writeline (y "- больше");
 writeline (x "-меньше");
+
+
+
+Readline(a,b,c);
+Console.WriteLine("Введите 3 числа:");
+            int num_1 = Convert.ToInt32(Console.ReadLine());
+            int num_2 = Convert.ToInt32(Console.ReadLine());
+            int num_3 = Convert.ToInt32(Console.ReadLine());
+
+            int max = num_1;
+
+            if (num_2 > max)
+            {
+                max = num_2;
+            }
+
+            if (num_3 > max)
+            {
+                max = num_3;
+            }
+
+            Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+            break;
+
+    
