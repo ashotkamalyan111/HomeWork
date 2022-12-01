231d603f9bf1561d7edd571a3fb06c435496e7f3
Ashot <adres1991@internet.ru>
2022-12-01 19:35:46 +0300

    первая задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Writeline ("Введите два рзличных числа);
+ReadLine (x,y);
+
+if x>y
+writeline (x "- больше");
+writeline (y " - меньше");
+
+else y>x
+writeline (y "- больше");
+writeline (x "-меньше");
