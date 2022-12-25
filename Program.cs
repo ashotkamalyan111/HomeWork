1374e9b4fa8d91cb238c8dd3c87f35c502739dc4
Ashot <adres1991@internet.ru>
2022-12-25 16:41:11 +0300

    HW

--- Program.cs
+++ Program.cs
@@ -1,32 +1,131 @@
-Console.Write("Введите числа через запятую: ");
-	int[] numbers = StringToNum(Console.ReadLine());
+Console.WriteLine("введите количество строк");
+	int linesVol = Convert.ToInt32(Console.ReadLine());
+	Console.WriteLine("введите количество столбцов");
+	int columnsVol = Convert.ToInt32(Console.ReadLine());
+	double[,] numbers = new double[linesVol, columnsVol];
+	FillArrayRandomNumbers(numbers);
 	PrintArray(numbers);
-	int sum = 0;
-	for (int i = 0; i < numbers.Length; i++)
+	
+	void FillArrayRandomNumbers(double[,] array)
 	{
-	if (numbers[i] > 0)
+	for (int i = 0; i < array.GetLength(0); i++)
 	{
-	sum++;
+	for (int j = 0; j < array.GetLength(1); j++)
+	{
+	array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
 	}
+	}
+	}
+	
+	void PrintArray(double[,] array)
+	{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+	Console.Write("[ ");
+	for (int j = 0; j < array.GetLength(1); j++)
+	{
+	Console.Write(array[i, j] + " ");
+	}
+	Console.Write("]");
+	Console.WriteLine("");
+	}
+	}
+
+
+
+
+
+Console.WriteLine("введите номер строки");
+	int n = Convert.ToInt32(Console.ReadLine());
+	Console.WriteLine("введите номер столбца");
+	int m = Convert.ToInt32(Console.ReadLine());
+	int [,] numbers = new int [10,10];
+	FillArrayRandomNumbers(numbers);
+	
+	if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+	{
+	Console.WriteLine("такого элемента нет");
+	}
+	else
+	{
+	Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+	}
+	
+	PrintArray(numbers);
+	
+	void FillArrayRandomNumbers(int[,] array)
+	{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{ 
+	for (int j = 0; j < array.GetLength(1); j++)
+	{
+	array [i,j] = new Random().Next(-100, 100)/10;
+	} 
+	}
+	}
+	
+	void PrintArray(int[,] array)
+	{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+	Console.Write("[ ");
+	for (int j = 0; j < array.GetLength(1); j++)
+	{
+	Console.Write(array[i,j] + " ");
+	} 
+	Console.Write("]");
+	Console.WriteLine(""); 
+	}
+	}
+
+
+
+Console.WriteLine("введите количество строк");
+	int n = Convert.ToInt32(Console.ReadLine());
+	Console.WriteLine("введите количество столбцов");
+	int m = Convert.ToInt32(Console.ReadLine());
+	
+	int[,] numbers = new int[n, m];
+	FillArrayRandomNumbers(numbers);
+	
+	
+	for (int j = 0; j < numbers.GetLength(1); j++)
+	{
+	double avarage = 0;
+	for (int i = 0; i < numbers.GetLength(0); i++)
+	{
+	avarage = (avarage + numbers[i, j]);
+	}
+	avarage = avarage / n;
+	Console.Write(avarage + "; ");
 	}
 	Console.WriteLine();
-	Console.WriteLine($"количество значений больше 0 = {sum}");
+	PrintArray(numbers);
 	
 	
 	
-
-
-
-    Console.WriteLine("введите значение b1");
-	double b1 = Convert.ToInt32(Console.ReadLine());
-	Console.WriteLine("введите число k1");
-	double k1 = Convert.ToInt32(Console.ReadLine());
-	Console.WriteLine("введите значение b2");
-	double b2 = Convert.ToInt32(Console.ReadLine());
-	Console.WriteLine("введите число k2");
-	double k2 = Convert.ToInt32(Console.ReadLine());
+	void FillArrayRandomNumbers(int[,] array)
+	{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+	for (int j = 0; j < array.GetLength(1); j++)
+	{
+	array[i, j] = new Random().Next(0, 10);
+	}
+	}
+	}
 	
-	double x = (-b2 + b1)/(-k1 + k2);
-	double y = k2 * x + b2;
+	void PrintArray(int[,] array)
+	{
 	
-	Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+	Console.Write("[ ");
+	for (int j = 0; j < array.GetLength(1); j++)
+	{
+	Console.Write(array[i, j] + " ");
+	}
+	Console.Write("]");
+	Console.WriteLine("");
+	}
+	}
