1372f46bf0f2a71a2f90678d8bc71e3a38cde578
Ashot <adres1991@internet.ru>
2023-02-20 23:33:01 +0300

    HW

--- Program.cs
+++ Program.cs
@@ -1,45 +1,71 @@
-using System;
+задача 1
 
-namespace ConsoleApplication1
-{
-    internal class Program
-    {
-        public static void Main()
-        {
-            string[] input = { "12", "aaaaaaa", "15", ".", "aa" };
-            var result = GetShortStrings(input);
 
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.Write(result[i] + " ");
-            }
-        }
+a = 123
+print(a//100 + a//10%10 + a%10)
 
-        public static string[] GetShortStrings(string[] inputArray)
-        {
-            int size = 0;
+# или
 
-            for (int i = 0; i < inputArray.Length; i++)
-            {
-                if (inputArray[i].Length <= 3)
-                {
-                    size++;
-                }
-            }
+num = input('Введите 3-х значное число: ')
+res = 0
+if len(num) == 3:
+    for i in num:
+        res += int(i)
+    print(res)
+else:
+    print('Вы ввели не 3-х значное число')
 
-            string[] resultArray = new string[size];
-            int j = 0;
-            
-            for (int i = 0; i < inputArray.Length; i++)
-            {
-                if (inputArray[i].Length <= 3)
-                {
-                    resultArray[j] = inputArray[i];
-                    j++;
-                }
-            }
 
-            return resultArray;
-        }
-    }
-}
+
+
+
+    задача 2 
+
+
+    s = 60
+print('Петя и Сережа сделали по', s/6, 'шт')
+print('Маша сделала ', s/6*4, 'шт')
+
+
+
+
+
+
+
+
+задача 3
+
+
+t = input('Введите номер билета: ')
+l = int(t[0]) + int(t[1]) + int(t[2])
+r = int(t[3]) + int(t[4]) + int(t[5])
+if l == r:
+    print('Yes')
+else:
+    print('NO')
+# или
+s = input('Введите 6-значный номер билета: ')
+if len(s) != 6:
+    print(f'Число {s} не 6-ти значное')
+else:
+    res1 = 0
+    res2 = 0
+    for i in range(len(s)//2):
+        res1 += int(s[i])
+        res2 += int(s[len(s)//2 + i])
+    if res1 == res2:
+        print(f'{s} счастливый номер')
+    else:
+        print(f'{s} не счастливый номер')
+
+
+
+        задача 4 
+
+
+        n,m,k = int(input('В-те 1-ю сторону: ')),int(input('В-те 2-ю сторону: ')),int(input('В-те кол-во долек: '))
+if k%n == 0 or k%m == 0:
+    print('Yes')
+else: print('No')
+# или
+if k < n*m and (k % n == 0 or k % m == 0):
