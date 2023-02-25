c5ab50db6c1cf7c532b9a7d23e3f543985b2c260
Ashot <adres1991@internet.ru>
2023-02-25 20:15:25 +0300

    HW2

--- Program.cs
+++ Program.cs
@@ -1,71 +1,63 @@
-задача 1
+задача 1 
 
 
-a = 123
-print(a//100 + a//10%10 + a%10)
-
-# или
-
-num = input('Введите 3-х значное число: ')
-res = 0
-if len(num) == 3:
-    for i in num:
-        res += int(i)
-    print(res)
-else:
-    print('Вы ввели не 3-х значное число')
-
-
-
-
-
-    задача 2 
-
-
-    s = 60
-print('Петя и Сережа сделали по', s/6, 'шт')
-print('Маша сделала ', s/6*4, 'шт')
-
-
-
-
-
-
-
-
-задача 3
-
-
-t = input('Введите номер билета: ')
-l = int(t[0]) + int(t[1]) + int(t[2])
-r = int(t[3]) + int(t[4]) + int(t[5])
-if l == r:
-    print('Yes')
-else:
-    print('NO')
-# или
-s = input('Введите 6-значный номер билета: ')
-if len(s) != 6:
-    print(f'Число {s} не 6-ти значное')
-else:
-    res1 = 0
-    res2 = 0
-    for i in range(len(s)//2):
-        res1 += int(s[i])
-        res2 += int(s[len(s)//2 + i])
-    if res1 == res2:
-        print(f'{s} счастливый номер')
+N = int(input('Введите количество монет '))
+orel = reshka = 0
+for i in range(N):
+    x = int(input('Орел(1) или решка(0)? '))
+    if x == 1:
+        orel += 1
     else:
-        print(f'{s} не счастливый номер')
+        reshka += 1
+if orel < reshka:
+    print(f'Переверните {orel} монет с орла на решку, их меньше всего')
+elif orel == reshka:
+    print(f'Количество орлов и решек одинаково, по {orel} штук')
+else:
+    print((f'Переверните {reshka} монет с решки на орла, их меньше всего'))
+
+
+    задача 2
+
+
+    x = abs(int(input('Введите первое натуральное число X ')))
+y = abs(int(input('Введите второе натуральное число Y ')))
+S = x + y
+P = x * y
+y1 = int((S + ((-S) ** 2 - 4 * P) ** 0.5) / 2)
+x1 = int((S - ((-S) ** 2 - 4 * P) ** 0.5) / 2)
+print(x1, y1)
 
 
 
-        задача 4 
+задача 3 
 
 
-        n,m,k = int(input('В-те 1-ю сторону: ')),int(input('В-те 2-ю сторону: ')),int(input('В-те кол-во долек: '))
-if k%n == 0 or k%m == 0:
-    print('Yes')
-else: print('No')
-# или
-if k < n*m and (k % n == 0 or k % m == 0):
+
+def main():
+    input_file = open("input.txt", "r")
+    output_file = open("output.txt", "w")
+    line = input_file.readline().split()
+    n = int(line[0])
+    k = 0
+    m = 0
+    for i in range(n):
+        line = input_file.readline().split()
+        if int(line[0]) == 0:
+            k = k + 1
+        if int(line[0]) == 1:
+            m = m + 1
+    print(m, k)
+ 
+    if m > k:
+        ans = k
+    else:
+        ans = m
+ 
+ 
+    print(ans)
+    output_file.write(str(ans) + "\n")
+ 
+ 
+if __name__ == "__main__":
+    main()
